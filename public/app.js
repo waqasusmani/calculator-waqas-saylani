5df9bb8ab317c338bd72c741043e2cbad3a5b0f0
Waqas Usmani <waqas-usmani@hotmail.com>
2020-06-27 17:50:06 +0000

    first commit

---
+++ public/app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.querySelector("#entry").value="";
+    document.querySelector("#result").value="";
+})
+
+var entrySting="0";
+
+function calculate(elem){
+    document.querySelector("#result").value=""
+    if (elem.id!="equals"){
+        entryString=document.querySelector("#entry").value;
+        entryString+=document.getElementById(elem.id).value;
+        document.querySelector("#entry").value=entryString;
+    }
+    console.log(entryString)
+    if (elem.id=="equals"){
+        document.getElementById("entry").value="";
+        if (elem.id!="plus" && elem.id!="minus" && elem.id!="multiply" && elem.id!="divide"){
+            document.getElementById("result").value=eval(entryString);
+        }
+    }
+    else {
+        if (elem.id!="plus" && elem.id!="minus" && elem.id!="multiply" && elem.id!="divide") {
+            document.getElementById("result").value=eval(entryString)
+        }
+        
+    }
+}
+
